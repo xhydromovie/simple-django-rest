483c76408189cbcb7abd7245323451da14deb7e9
xhydromovie <xhydromovie@gmail.com>
2019-02-19 18:01:10 +0100

    Added some views

--- myrest/myrest/urls.py
+++ myrest/myrest/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import views
+from .api import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello, name="hello"),
+    path('api/v1/', include(router.urls)),
 ]
